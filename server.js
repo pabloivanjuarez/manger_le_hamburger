dbc3617d18b113d3328f9fbfd967362ff86bdf88
pablito <pabloivanjuarez@gmail.com>
2020-10-11 17:10:07 -0700

    server updated, now listening(locally) on 8080

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 8080;
+
+const app = express()
+
+// static content set to public
+app.use(express.static('public'));
+
+// parse app body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// handlebars handler
+let expH = require('express-handlebars')
+
+app.engine = require('handlebars', expH({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// import routes, server given access
+let routes = require('./controllers/burgers_controller');
+
+app.use(routes);
+
+// sever start
+app.listen(PORT, function (){
+  //  server log, to show when successful start
+  console.log('server listening on:' + PORT);
+})
