9c05a4aec8d223ba1db6f202bd5206326e4c319d
pablito <pabloivanjuarez@gmail.com>
2020-10-12 21:37:13 -0700

    enabled connection to be used in cloud

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // set up MySQL connection
 const mysql=require('mysql');
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'ceo',
-  password: 'leboss321',
-  database: 'burger_db'
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "ceo",
+    password: "leboss321",
+    database: "burger_db",
+  });
+}
 
 // make connection
 connection.connect(function(err){
