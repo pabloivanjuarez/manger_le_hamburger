9fdf0dc970e90a5079c10d6bd653ee1bee99adbc
pablito <pabloivanjuarez@gmail.com>
2020-10-12 18:28:59 -0700

    connection threadId updated

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,7 @@ connection.connect(function(err){
     console.error('error connecting: ' + err.stack);
   return;  
   }
-  console.log('connected as id ' + connection.threadID);
+  console.log('connected as id ' + connection.threadId);
 });
 
 // export connection 
