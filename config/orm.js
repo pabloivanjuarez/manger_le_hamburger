70774e9b9d58340d09eef17eecd3cd2c84d15d44
pablito <pabloivanjuarez@gmail.com>
2020-10-15 21:28:19 -0700

    orm handler updated

--- config/orm.js
+++ config/orm.js
@@ -6,7 +6,7 @@ function printQuestionMarks(num) {
   let arr = [];
 
   for (let i = 0; i < num; i++) {
-arr.push('?');    
+    arr.push('?');
   }
   return arr.toString();
 }
@@ -16,15 +16,15 @@ function objToSql(ob) {
   let arr = [];
 
   // loop through the keys and push the key/value as a string int arr
-  for (var key in object) {
+  for (var key in ob) {
     var value = ob[key];
     // vheck to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
       // if string w/ spaces, add quotations
-     if (typeof value === 'string' && value.indexOf(" ") >= 0) {
-       value = "'" + value + "'";
-    }
-    arr.push(key + "=" + value);
+      if (typeof value === 'string' && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
     }
   }
   return arr.toString();
@@ -35,36 +35,36 @@ var orm = {
   all: function (table, cb) {
     var queryString = 'SELECT * FROM ' + table + ';';
     console.log(queryString);
-    connection.query(queryString, function (err, result){
-      console.table(result);     
-      if (err) { 
+    connection.query(queryString, function (err, result) {
+      console.table(result);
+      if (err) {
         throw err;
       }
-       cb(result);
+      cb(result);
     });
   },
-    create: function(table, cols, cb) {
-      var queryString = 'INSERT INTO '+ table;
-      queryString += ' (';
-      queryString += cols.toString();
-      queryString += ') ';
-      queryString += 'VALUES (';
-      queryString += printQuestionMarks(vals.length);
-      queryString += ') ';
-      
-      console.log(queryString);
-      
-      connection.query(queryString, vals, function(err, res){
-        if (err) {
-          throw err;
-        }
-        cb(res);
-      });
-    },
+  create: function (table, cols, vals, cb) {
+    var queryString = 'INSERT INTO ' + table;
+    queryString += ' (';
+    queryString += cols.toString();
+    queryString += ') ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(vals.length);
+    queryString += ') ';
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
 
   // an example of objColVals would be {name: killer burger, devoured: true}
-  update: function(table, objColVals, condidtion, cb) {
-    var queryString = 'UPDATE'+ table;
+  update: function (table, objColVals, condidtion, cb) {
+    var queryString = 'UPDATE ' + table;
 
     queryString += ' SET ';
     queryString += objToSql(objColVals);
@@ -72,19 +72,19 @@ var orm = {
     queryString += condidtion;
 
     console.log(queryString);
-     connection.query(queryString, function(err, res){
-       if (err) {
-         throw err;
-       }
-       cb(res);
-     });
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
   },
   delete: function (table, condition, cb) {
     var queryString = 'DELETE FROM ' + table;
     queryString += ' WHERE ';
     queryString += condition;
 
-    connection.query(queryString, function(err, res){
+    connection.query(queryString, function (err, res) {
       if (err) {
         throw err;
       }
