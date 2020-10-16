c1a40d556bbdb62512182605030f53143861d59e
pablito <pabloivanjuarez@gmail.com>
2020-10-15 21:29:17 -0700

    routes cleaned up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 //create routes/ set up logic within routes, where needed
-router.get('/', function(req, res){
-  burger.all(function(data){
-    var hbsObject={
-      burgers: data
+router.get('/', function (req, res) {
+  burger.all(function (data) {
+    var hbsObject = {
+      burger: data
     };
     res.render('index', hbsObject);
   });
@@ -16,35 +16,37 @@ router.get('/', function(req, res){
 
 
 // recall all burgers
-router.post('/api/plate', function(req,res) {
-  burger.create(['burger_name'], [req.body.name], function(result){
-    res.json({ id: result.insertId}); 
+router.post('/api/plate', function (req, res) {
+  burger.create(['burger_name'], [req.body.name], function (result) {
+    res.json({
+      id: result.insertId
+    });
   });
 });
 
 // change burger status function
-router.put('/api/plate/:id', function(req,res){
+router.put('/api/plate/:id', function (req, res) {
   var condition = 'id = ' + req.params.id;
 
   burger.update({
-    devoured: req.body.devoured,
-  }, condition,
-    function (result){
+      devoured: req.body.devoured,
+    }, condition,
+    function (result) {
       if (result.changedRows == 0) {
-        return res.status(4040).end();
+        return res.status(404).end();
       } else {
         res.status(200).end();
       }
     }
   );
 });
- 
+
 //  rm burger function
-router.delete('/api/plate/:id', function(req,res){
+router.delete('/api/plate/:id', function (req, res) {
   var condition = 'id = ' + req.params.id;
 
-  burger.delete(condition, function (result){
-    if (result.affectedRows == 0){
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
       return res.status(404).end();
     } else {
       res.status(200).end();
